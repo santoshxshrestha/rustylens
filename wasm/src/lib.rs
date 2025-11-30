920357fcac602bf4cc784dbcb88992c2abc1e450
santoshxshrestha <santoshxshrestha@gmail.com>
2025-11-30 22:44:52 +0545

    wasm: init

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    log(&format!("Hello, {}!", name));
 }
